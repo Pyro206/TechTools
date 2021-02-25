921792b2450451504ef90b0cb22d0d553f264fe1
Sebastian Siljuholtet Johansen <aurilisdev@gmail.com>
2021-02-25 03:50:59 +0100

    Improve

--- src/main/java/common/items/tools/TechToolClasses.java
+++ src/main/java/common/items/tools/TechToolClasses.java
@@ -6,36 +6,14 @@ import utill.itemdirec;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.crafting.Ingredient;
 
-public enum TechToolClasses implements IItemTier{
-    STEEL(2, 1200, 4.0F, 5.0f, 5, () -> {
-
-        return Ingredient.fromItems(itemdirec.steel_pickaxe);
-        
-    }), BRONZE(2, 500, 3.0F, 5.0f, 5, () -> {
-
-        return Ingredient.fromItems(itemdirec.bronze_sword); }),
-    
-    TIN(2, 260, 3.0F, 5.0f, 5, () -> {
-
-        return Ingredient.fromItems(itemdirec.tin_sword); }),
-    
-    COPPER(2, 400, 3.0F, 5.0f, 5, () -> {
-
-        return Ingredient.fromItems(itemdirec.copper_sword); }),
-    
-    LEAD(1, 250, 2.0F, 5.0f, 5, () -> {
-
-        return Ingredient.fromItems(itemdirec.lead_sword); }),
-    
-    VANADIUM(4, 1000, 6.0F, 6.0f, 5, () -> {
-
-        return Ingredient.fromItems(itemdirec.vanadium_sword); }), 
-    
-    SILVER(3, 600, 5.0F, 5.0f, 5, () -> {
-
-            return Ingredient.fromItems(itemdirec.silver_pickaxe);
-            
-        });
+public enum TechToolClasses implements IItemTier {
+    STEEL(2, 1200, 4.0F, 5.0f, 5, () -> { return Ingredient.fromItems(itemdirec.steel_pickaxe); }),
+    BRONZE(2, 500, 3.0F, 5.0f, 5, () -> { return Ingredient.fromItems(itemdirec.bronze_sword); }),
+    TIN(2, 260, 3.0F, 5.0f, 5, () -> { return Ingredient.fromItems(itemdirec.tin_sword); }),
+    COPPER(2, 400, 3.0F, 5.0f, 5, () -> { return Ingredient.fromItems(itemdirec.copper_sword); }),
+    LEAD(1, 250, 2.0F, 5.0f, 5, () -> { return Ingredient.fromItems(itemdirec.lead_sword); }),
+    VANADIUM(4, 1000, 6.0F, 6.0f, 5, () -> { return Ingredient.fromItems(itemdirec.vanadium_sword); }),
+    SILVER(3, 600, 5.0F, 5.0f, 5, () -> { return Ingredient.fromItems(itemdirec.silver_pickaxe); });
 	
 
     private final int harvestLevel;
@@ -45,7 +23,7 @@ public enum TechToolClasses implements IItemTier{
     private final int enchantability;
     private final Supplier<Ingredient>reapairMat;
 
-    TechToolClasses(int harvestLevel, int maxUses, float efficency, float attackDammage, int enchantability, Supplier<Ingredient>reapairMat){
+    public TechToolClasses(int harvestLevel, int maxUses, float efficency, float attackDammage, int enchantability, Supplier<Ingredient>reapairMat) {
         this.harvestLevel = harvestLevel;
         this.maxUses = maxUses;
         this.efficency = efficency;
@@ -56,37 +34,31 @@ public enum TechToolClasses implements IItemTier{
 
     @Override
     public int getMaxUses() {
-        // TODO Auto-generated method stub
         return maxUses;
     }
 
     @Override
     public float getEfficiency() {
-        // TODO Auto-generated method stub
         return efficency;
     }
 
     @Override
     public float getAttackDamage() {
-        // TODO Auto-generated method stub
         return attackDammage;
     }
 
     @Override
     public int getHarvestLevel() {
-        // TODO Auto-generated method stub
-        return    harvestLevel;
+        return harvestLevel;
     }
 
     @Override
     public int getEnchantability() {
-        // TODO Auto-generated method stub
         return enchantability;
     }
 
     @Override
     public Ingredient getRepairMaterial() {
-        // TODO Auto-generated method stub
         return  reapairMat.get();
     }
 
